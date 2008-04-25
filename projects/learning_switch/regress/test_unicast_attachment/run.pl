168917f37bdffa89eb5c72dde6f8551e6c0c5f65
Masayoshi Kobayashi <mkobaya1@stanford.edu>
2008-04-25 20:00:32 +0000

    tests by Peyman

---
+++ projects/learning_switch/regress/test_unicast_attachment/run.pl
@@ -0,0 +1,143 @@
+#!/usr/bin/perl -w
+
+use NF2::TestLib;
+use NF2::PacketLib;
+use OF::OFUtil;
+use Error qw(:try);
+use IO::Socket;
+use strict;
+
+# sending/receiving interfaces - NOT OpenFlow ones
+my @interfaces = ("eth5", "eth6", "eth7", "eth8");
+
+my (%init_counters, %final_counters, %delta);
+
+my $pid;
+
+# Fork off a process for controller
+if ( !( $pid = fork ) ) {
+
+        # Run controller from this process
+        exec "controller", "-v", "nl:0";
+        die "Failed to launch controller: $!";
+}
+else {
+        my $exitCode = 1;
+        try {
+		# Run control from this process
+		print "added controller...\n";
+		
+		# Wait for contorller to load
+		sleep(1);		
+
+		# launch PCAP listenting interface
+		nftest_init(\@ARGV,\@interfaces,);
+		nftest_start(\@interfaces,);
+
+		# host A's MAC address is 00:00:00:00:00:01
+
+		my $pkt_args = {
+			DA => "00:00:00:00:00:02",
+			SA => "00:00:00:00:00:01",
+			src_ip => "192.168.0.40",
+			dst_ip => "192.168.1.40",
+			ttl => 64,
+			len => 64
+		};
+		my $pkt = new NF2::IP_pkt(%$pkt_args);
+
+		save_counters(\%init_counters);
+
+		# send one packet; controller should learn MAC, add a flow 
+		#  entry, and send this packet out the other interfaces
+		print "Sending now: \n";
+		send_and_count('eth5', $pkt->packed, \%delta);
+        expect_and_count('eth6', $pkt->packed, \%delta);
+        expect_and_count('eth7', $pkt->packed, \%delta);
+        expect_and_count('eth8', $pkt->packed, \%delta);
+
+		# sleep as long as needed for the test to finish
+		sleep 0.5;
+		
+		my $pkt_args = {
+			DA => "00:00:00:00:00:01",
+			SA => "00:00:00:00:00:02",
+			src_ip => "192.168.1.40",
+			dst_ip => "192.168.0.40",
+			ttl => 64,
+			len => 64
+		};
+		my $pkt = new NF2::IP_pkt(%$pkt_args);
+		send_and_count('eth6', $pkt->packed, \%delta);
+        expect_and_count('eth5', $pkt->packed, \%delta);
+        sleep 0.5;
+        
+        #Now Host A Has Changed Location and Attached to p2
+        #It will send a packet to p1 form its new location
+        
+ 		my $pkt_args = {
+			DA => "00:00:00:00:00:02",
+			SA => "00:00:00:00:00:01",
+			src_ip => "192.168.2.40",
+			dst_ip => "192.168.1.40",
+			ttl => 64,
+			len => 64
+		};
+		my $pkt = new NF2::IP_pkt(%$pkt_args);
+		send_and_count('eth7', $pkt->packed, \%delta);
+        expect_and_count('eth6', $pkt->packed, \%delta);
+        sleep 0.5;
+
+		# Now p1 sends something to Host A which is now attached to p2
+		# we expect the switch to already updated its entry for Host A
+
+		my $pkt_args = {
+			DA => "00:00:00:00:00:01",
+			SA => "00:00:00:00:00:02",
+			src_ip => "192.168.1.40",
+			dst_ip => "192.168.2.40",
+			ttl => 64,
+			len => 64
+		};
+		my $pkt = new NF2::IP_pkt(%$pkt_args);
+		send_and_count('eth6', $pkt->packed, \%delta);
+        expect_and_count('eth7', $pkt->packed, \%delta);
+        sleep 0.5;
+
+		#print "about to nftest_finish()\n";
+		my $unmatched_hoh = nftest_finish();
+
+		print "Checking pkt errors\n";
+		my $total_errors = nftest_print_errors($unmatched_hoh);
+
+		# check counter values 
+		save_counters(\%final_counters);
+		$total_errors += verify_counters(%init_counters, %final_counters, %delta);		
+
+                if ( $total_errors == 0 ) {
+                        print "SUCCESS!\n";
+                        $exitCode = 0;
+                }
+                else {
+                        print "FAIL: $total_errors errors\n";
+                        $exitCode = 1;
+                }
+
+        }
+        catch Error with {
+
+                # Catch and print any errors that occurred during control processing
+                my $ex = shift;
+                if ($ex) {
+                        print $ex->stringify();
+                }
+        }
+        finally {
+
+                # Ensure controller killed even if we have an error
+                kill 9, $pid;
+
+                # Exit with the resulting exit code
+                exit($exitCode);
+        };
+}
