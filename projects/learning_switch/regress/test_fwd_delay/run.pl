7c044e758be9391b68bad1797ea3c4bb0ee0054a
Masayoshi Kobayashi <mkobaya1@stanford.edu>
2008-05-05 01:39:38 +0000

    delay test by peyman

--- projects/learning_switch/regress/test_fwd_delay/run.pl
+++ projects/learning_switch/regress/test_fwd_delay/run.pl
@@ -1,156 +1,65 @@
 #!/usr/bin/perl -w
+#test_unicast_unknown
 
 use NF2::TestLib;
 use NF2::PacketLib;
 use OF::OFUtil;
-use Error qw(:try);
-use IO::Socket;
-use Time::HiRes qw(sleep gettimeofday tv_interval usleep);
 use strict;
+use Time::HiRes qw(sleep gettimeofday tv_interval usleep);
 
-# sending/receiving interfaces - NOT OpenFlow ones
-my @interfaces = ("eth5", "eth6", "eth7", "eth8");
+sub my_test {
+	my $cnt = 0;
 
-my (%init_counters, %final_counters, %delta);
+	my @start_time = gettimeofday();
+	my %delta;
+    for (my $t = 10; $t < 20; $t ++){
+     for ($cnt = 10; $cnt < 100; $cnt ++){
+	my $pkt_args = {
+		DA     => "00:01:00:00:$t:$cnt",
+		SA     => "00:00:00:00:00:01",
+		src_ip => "192.168.0.40",
+		dst_ip => "192.168.$cnt.$t",
+		ttl    => 64,
+		len    => 64
+	};
+	my $pkt = new NF2::IP_pkt(%$pkt_args);
 
-my $pid;
+	
+	# send one packet; controller should learn MAC, add a flow
+	#  entry, and send this packet out the other interfaces
 
-# Fork off a process for controller
-if ( !( $pid = fork ) ) {
-
-        # Run controller from this process
-        exec "controller", "-v", "nl:0";
-        die "Failed to launch controller: $!";
+	send_and_count( nftest_get_iface('eth1'), $pkt->packed, \%delta );
+	expect_and_count( nftest_get_iface('eth2'), $pkt->packed, \%delta );
+	expect_and_count( nftest_get_iface('eth3'), $pkt->packed, \%delta );
+	expect_and_count( nftest_get_iface('eth4'), $pkt->packed, \%delta );
 }
-else {
-        my $exitCode = 1;
-        try {
-		# Run control from this process
-		print "added controller...\n";
-		
-		# Wait for contorller to load
-		sleep(1);		
-
-		# launch PCAP listenting interface
-		nftest_init(\@ARGV,\@interfaces,);
-		nftest_start(\@interfaces,);
-
-		my $pkt_args = {
-			DA => "00:00:00:00:00:02",
-			SA => "00:00:00:00:00:01",
-			src_ip => "192.168.0.40",
-			dst_ip => "192.168.1.40",
-			ttl => 64,
-			len => 64
-		};
-		my $pkt = new NF2::IP_pkt(%$pkt_args);
-
-		save_counters(\%init_counters);
-
-		# send one packet; controller should learn MAC, add a flow 
-		#  entry, and send this packet out the other interfaces
-		print "Sending now: \n";
-		my @start_time = gettimeofday();
-		send_and_count('eth5', $pkt->packed, \%delta);
-                expect_and_count('eth6', $pkt->packed, \%delta);
-		my @load_time = tv_interval(\@start_time);
-		print "Time required for sending to an unknown destination: @load_time \n";
-
-                expect_and_count('eth7', $pkt->packed, \%delta);
-                expect_and_count('eth8', $pkt->packed, \%delta);
-
-
-		# sleep as long as needed for the test to finish
-		sleep 0.5;
-		
-		my $pkt_args = {
-			DA => "00:00:00:00:00:01",
-			SA => "00:00:00:00:00:02",
-			src_ip => "192.168.1.40",
-			dst_ip => "192.168.0.40",
-			ttl => 64,
-			len => 64
-		};
-		my $pkt = new NF2::IP_pkt(%$pkt_args);
-		@start_time = gettimeofday();
-
-		send_and_count('eth6', $pkt->packed, \%delta);
-                expect_and_count('eth5', $pkt->packed, \%delta);
-		@load_time = tv_interval(\@start_time);
-		print "Time required for sending to a known destination: @load_time \n";
-
-        sleep 0.5;
-        
- 		my $pkt_args = {
-			DA => "00:00:00:00:00:01",
-			SA => "00:00:00:00:00:03",
-			src_ip => "192.168.2.40",
-			dst_ip => "192.168.0.40",
-			ttl => 64,
-			len => 64
-		};
-		my $pkt = new NF2::IP_pkt(%$pkt_args);
-		@start_time = gettimeofday();
-		send_and_count('eth7', $pkt->packed, \%delta);
-                expect_and_count('eth5', $pkt->packed, \%delta);
-		@load_time = tv_interval(\@start_time);
-		print "Time required for sending to a known destination: @load_time \n";
-
-
-        sleep 0.5;
-
-		my $pkt_args = {
-			DA => "00:00:00:00:00:01",
-			SA => "00:00:00:00:00:04",
-			src_ip => "192.168.3.40",
-			dst_ip => "192.168.0.40",
-			ttl => 64,
-			len => 64
-		};
-		my $pkt = new NF2::IP_pkt(%$pkt_args);
-		@start_time = gettimeofday();
-		send_and_count('eth8', $pkt->packed, \%delta);
-                expect_and_count('eth5', $pkt->packed, \%delta);
-		@load_time = tv_interval(\@start_time);
-		print "Time required for sending to a known destination: @load_time \n";
-
-
-        sleep 0.5;
-
-		#print "about to nftest_finish()\n";
-		my $unmatched_hoh = nftest_finish();
-
-		print "Checking pkt errors\n";
-		my $total_errors = nftest_print_errors($unmatched_hoh);
-
-		# check counter values 
-		save_counters(\%final_counters);
-		$total_errors += verify_counters(%init_counters, %final_counters, %delta);		
-
-                if ( $total_errors == 0 ) {
-                        print "SUCCESS!\n";
-                        $exitCode = 0;
-                }
-                else {
-                        print "FAIL: $total_errors errors\n";
-                        $exitCode = 1;
-                }
-
-        }
-        catch Error with {
-
-                # Catch and print any errors that occurred during control processing
-                my $ex = shift;
-                if ($ex) {
-                        print $ex->stringify();
-                }
-        }
-        finally {
-
-                # Ensure controller killed even if we have an error
-                kill 9, $pid;
-
-                # Exit with the resulting exit code
-                exit($exitCode);
-        };
 }
+	my @load_time1 = tv_interval(\@start_time);
+
+
+	my @start_time = gettimeofday();
+     for ($cnt = 10; $cnt < 20; $cnt ++){
+     for (my $t = 10; $t < 100; $t ++){
+	my $pkt_args = {
+		DA     => "00:00:00:00:00:01",
+		SA     => "00:00:00:00:$t:$cnt",
+		src_ip => "192.168.$t.$cnt",
+		dst_ip => "192.168.1.40",
+		ttl    => 64,
+		len    => 64
+	};
+	my $pkt = new NF2::IP_pkt(%$pkt_args);
+
+	send_and_count( nftest_get_iface('eth2'), $pkt->packed, \%delta );
+	expect_and_count( nftest_get_iface('eth1'), $pkt->packed, \%delta );
+}
+}
+	my @load_time = tv_interval(\@start_time);
+	print "Dealy with unknown MAC for 1000 pkts: @load_time1\n";
+	print "Dealy with known MAC for 1000 pkts: @load_time\n";
+
+	return %delta;
+}
+
+# how do we pass the cmd-line arguments to the script?
+run_learning_switch_test( \&my_test );
