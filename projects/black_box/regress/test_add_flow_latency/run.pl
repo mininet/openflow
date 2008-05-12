6a3d2f2531d3fed73d13c5d8f36c19398539cc80
Masayoshi Kobayashi <mkobaya1@stanford.edu>
2008-05-12 10:34:29 +0000

    small fixes to test_add_flow_latency

--- projects/black_box/regress/test_add_flow_latency/run.pl
+++ projects/black_box/regress/test_add_flow_latency/run.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-# test_add_flow_bandwidth
+# test_add_flow_latency
 
 use strict;
 use IO::Socket;
@@ -30,18 +30,12 @@ sub my_test {
 
 	my $test_pkt = new NF2::UDP_pkt(%$pkt_args);
 
-	## ip_hdr_len is not correctly set by NF2 lib, so do it here.
-	my $iphdr=$test_pkt->{'IP_hdr'};
-	$$iphdr->ip_hdr_len(5); #set ip_hdr_len
-
-
-
-
 	my $wildcards = 0x0;
 	my $in_port = 1;
 	my $out_port = 2;
+	my $max_idle = 0;
 
-	my $flow_mod_pkt = create_flow_mod_from_udp($ofp,$test_pkt,$in_port,$out_port,0,0);
+	my $flow_mod_pkt = create_flow_mod_from_udp($ofp,$test_pkt,$in_port,$out_port,$max_idle,$wildcards);
 
 	print $sock $flow_mod_pkt;
 	usleep(1000000);
@@ -49,7 +43,6 @@ sub my_test {
 
 	my $cnt = 0;
 	my $start_time = [gettimeofday()];
-	my %delta;
 	for( $cnt = 0;$cnt < 1000; $cnt++){
 	    nftest_send( nftest_get_iface( "eth" . ($in_port+1)),$test_pkt->packed );
 	    nftest_expect( nftest_get_iface( "eth" . ($out_port+1)),$test_pkt->packed );
