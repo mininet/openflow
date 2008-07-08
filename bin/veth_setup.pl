1b7beb1ab67c640042d2b2efe405360b59cc3b09
Brandon Heller <brandonh@stanford.edu>
2008-07-08 00:30:13 +0000

    minor changes

--- bin/veth_setup.pl
+++ bin/veth_setup.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl -w
 
-`modprobe veth`;
+`/sbin/modprobe veth`;
 for (my $i = 0; $i < 4; $i++) {
-	`ip link add type veth`;
+	`/sbin/ip link add type veth`;
 }
 
 for (my $i = 0; $i < 8; $i++) {
-	`sudo ifconfig veth$i 192.168.1$i.1 netmask 255.255.255.0`;
+	`sudo /sbin/ifconfig veth$i 192.168.1$i.1 netmask 255.255.255.0`;
 }
