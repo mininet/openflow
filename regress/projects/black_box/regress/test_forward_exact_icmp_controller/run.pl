a28d122c9091ab3aac35f60f605f25f7a770d3c9
Tatsuya Yabe <tyabe@stanford.edu>
2009-01-11 23:26:55 -0800

    Add regression tests for ICMP handling feature Byte swap for ICMP entry field on FPGA bitfile

---
+++ regress/projects/black_box/regress/test_forward_exact_icmp_controller/run.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w
+# test_forward_exact_icmp_controller
+
+use strict;
+use OF::Includes;
+
+sub forward_controller {
+
+	forward_simple_icmp(@_, 'controller', 0);  # 0: fool_flg = off
+}
+
+sub my_test {
+
+	my ( $sock, $options_ref ) = @_;
+
+	enable_flow_expirations( $ofp, $sock );
+
+	for_all_ports( $ofp, $sock, $options_ref, \&forward_controller, 0x0);
+}
+
+run_black_box_test( \&my_test, \@ARGV );
