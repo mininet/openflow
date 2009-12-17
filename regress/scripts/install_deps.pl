b5f91a1a01d5d82fcd5572b30110d95d4d3fbf3e
Glen Gibb <grg@stanford.edu>
2009-12-16 17:14:02 -0800

    regress: automate dependency installation
    Provide script to automatically install dependencies necessary for
    running regression tests. Currently supported OSes:
     - Ubuntu

---
+++ regress/scripts/install_deps.pl
@@ -0,0 +1,200 @@
+#!/usr/bin/perl -W
+#
+# Script to automatically install dependencies for regression tests
+
+use strict;
+use File::Basename;
+use File::Path;
+use Getopt::Std;
+use Cwd;
+
+use constant {
+	UBUNTU  => 'Ubuntu',
+	DEBIAN  => 'Debian',
+	REDHAT  => 'RedHat',
+	FEDORA  => 'Fedora',
+};
+
+# Executables
+my $lsb_release = 'lsb_release';
+my $apt_get = 'apt-get';
+my $yum = 'yum';
+
+my $distro;
+my $sim;
+my %install_funcs = (
+	'Ubuntu'  => \&install_ubuntu,
+);
+our($opt_s, $opt_d);
+
+# Verify that this script is being run as root
+if ($> != 0) {
+	die "This script must be run as root";
+}
+
+# Parse the command line arguments
+parse_args();
+
+
+# Identify the distribution
+if (!defined($distro)) {
+	identify_distro();
+	die "Unable to identify the distribution" if (!defined($distro));
+}
+
+# Call the appropriate install function
+if ($install_funcs{$distro}) {
+	$install_funcs{$distro}->();
+}
+else {
+	die "Unable to find the install function for '$distro'";
+}
+
+exit 0;
+
+#==========================================================
+
+#
+# identify_distro:
+#   Attempt to identify the Linux distro
+#
+sub identify_distro {
+	# First, look for lsb release which makes querying easier
+	$lsb_release = `which $lsb_release`;
+	chomp($lsb_release);
+	if ( $? >> 8 == 0) {
+		$distro = `$lsb_release -s -i`;
+		chomp($distro);
+		SWITCH: for ($distro) {
+			/Ubuntu/ && do {
+				$distro = UBUNTU;
+				last SWITCH;
+			};
+
+			/Debian/ && do {
+				$distro = DEBIAN;
+				last SWITCH;
+			};
+
+			(/CentOS/ || /RedHat/) && do {
+				$distro = REDHAT;
+				last SWITCH;
+			};
+
+			/Fedora/ && do {
+				$distro = FEDORA;
+				last SWITCH;
+			};
+
+			# DEFAULT
+			warn "Unknown Linux distro '$distro'";
+			$distro = undef;
+		}
+	}
+
+	# Otherwise, fall back to looking for release/version files in /etc
+	else {
+		if ( -f '/etc/debian_version' || -f '/etc/debian_release' ) {
+			$distro = DEBIAN;
+		}
+		elsif ( -f '/etc/fedora-release') {
+			$distro = FEDORA;
+		}
+		elsif ( -f '/etc/redhat-release' || -f '/etc/redhat_release' ) {
+			$distro = REDHAT;
+		}
+	}
+}
+
+#
+# install_ubuntu:
+#   Install the necessary dependencies
+#
+sub install_ubuntu {
+	my @pkgs = (
+		'liberror-perl',
+		'libio-interface-perl',
+		'liblist-moreutils-perl',
+		'libpcap0.8-dev',
+		'iproute',
+		'psmisc',
+		'libconvert-binary-c-perl',
+		'libnet-pcap-perl',
+		'libnet-rawip-perl',
+		'wget',
+	);
+
+	my @modules = (
+		'http://search.cpan.org/CPAN/authors/id/F/FT/FTASSIN/Data-HexDump-0.02.tar.gz',
+		'http://www.cpan.org/authors/id/J/JV/JV/Getopt-Long-2.38.tar.gz',
+	);
+
+	# Run apt-get
+	my @flags = ('-y');
+	push(@flags, '-s') if defined($sim);
+	system($apt_get, @flags, 'install', @pkgs);
+	if ($? >> 8 != 0) {
+		die "Error running $apt_get";
+	}
+
+	# Install modules directly from CPAN
+	install_perl_modules(@modules);
+}
+
+
+#
+# install_perl_modules:
+#   Fetch and install PERL modules
+#
+sub install_perl_modules {
+	my @modules = @_;
+
+	my $dir = "perl_modules";
+
+	mkdir $dir;
+	chdir $dir;
+
+	foreach my $path (@modules) {
+		`wget $path`;
+		my $module = fileparse($path);
+		`tar xzf $module`;
+		$module =~ s/.tar.gz//;
+		print "compiling $module\n";
+		chdir $module;
+		if (!defined($sim)) {
+			system 'perl Makefile.PL';
+			system 'make';
+			system 'make install';
+		}
+		chdir '../';
+	}
+
+	chdir '..';
+	rmtree $dir;
+}
+
+#
+# parse_args
+#   Parse the command line arguments
+#
+sub parse_args {
+	getopts('sd:');
+	$sim = 1 if defined($opt_s);
+	$distro = $opt_d if defined($opt_d);
+}
+
+sub HELP_MESSAGE {
+	print <<USAGE;
+Usage:
+    $0 [-s] [-d distro]
+
+where
+    -s   Run the utility in simulation mode
+
+    -d distro
+         Specify the distro instead of trying to identify it. Known distros:
+USAGE
+
+	print "           " . join(', ', sort(keys(%install_funcs))) . "\n";
+	exit 0;
+}
